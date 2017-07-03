c515f4bb2c28cb681eae4c9decabc4e9cbec3368
zyl-githab <zhuaynlong1819@163.com>
2017-07-03 15:42:28 +0800

    修改警告
    修改警告

--- test-manager/manager-web/src/main/java/com/happy/controller/like/UserController.java
+++ test-manager/manager-web/src/main/java/com/happy/controller/like/UserController.java
@@ -4,26 +4,19 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.happy.business.User;
 import com.happy.business.service.UserService;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 
 /**
  * 
